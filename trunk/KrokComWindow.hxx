e55d3efa651fe20f5ecd948efc5cad2846dcdba4
stephena <stephena@45934ac9-492d-464f-bfc4-cf106a11c228>
2009-05-22 10:55:21 +0000

    Initial import of KrokCom code.

---
+++ trunk/KrokComWindow.hxx
@@ -0,0 +1,60 @@
+//============================================================================
+//
+//  K   K  RRRR    OOO   K   K   CCCC   OOO   M   M
+//  K  K   R   R  O   O  K  K   C      O   O  MM MM
+//  KKK    RRRR   O   O  KKK    C      O   O  M M M  "Krokodile Cart software"
+//  K  K   R R    O   O  K  K   C      O   O  M   M
+//  K   K  R  R    OOO   K   K   CCCC   OOO   M   M
+//
+// Copyright (c) 2009 by Stephen Anthony <stephena@users.sourceforge.net>
+//
+// See the file "License.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//
+// $Id$
+//============================================================================
+
+#ifndef __KROKCOM_WINDOW_HXX
+#define __KROKCOM_WINDOW_HXX
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QProgressBar>
+
+#include "Cart.hxx"
+#include "SerialPortManager.hxx"
+#include "ui_krokcomwindow.h"
+
+namespace Ui
+{
+  class KrokComWindow;
+}
+
+class KrokComWindow : public QMainWindow
+{
+Q_OBJECT
+  public:
+    KrokComWindow(QWidget* parent = 0);
+    ~KrokComWindow();
+
+  private:
+    void setupConnections();
+
+  private slots:
+    void slotConnectKrokCart();
+    void slotOpenROM();
+    void slotDownloadROM();
+    void slotVerifyROM();
+
+  private:
+    Ui::KrokComWindow* ui;
+
+    Cart myCart;
+    SerialPortManager myManager;
+
+    QLabel* myStatus;
+    QLabel* myLED;
+    QProgressBar* myProgress;
+};
+
+#endif
