da258b16621e7ade5a6c02a62edc2e38fcbfee7c
stephena <stephena@45934ac9-492d-464f-bfc4-cf106a11c228>
2009-11-29 03:17:44 +0000

    Added a custom AboutDialog class, which includes a scrollable TextBrowser. Now that the About dialog can show more text, also included a credits list of all people who helped during the development of KrokCom.

---
+++ trunk/AboutDialog.hxx
@@ -0,0 +1,37 @@
+//===========================================================================
+//
+//  K   K  RRRR    OOO   K   K   CCCC   OOO   M   M
+//  K  K   R   R  O   O  K  K   C      O   O  MM MM
+//  KKK    RRRR   O   O  KKK    C      O   O  M M M  "Krokodile Cart software"
+//  K  K   R R    O   O  K  K   C      O   O  M   M
+//  K   K  R  R    OOO   K   K   CCCC   OOO   M   M
+//
+// Copyright (c) 2009 by Stephen Anthony <stephena@users.sourceforge.net>
+//
+// See the file "License.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//
+// $Id$
+//============================================================================
+
+#ifndef ABOUTDIALOG_HXX
+#define ABOUTDIALOG_HXX
+
+#include <QtGui/QDialog>
+
+namespace Ui {
+  class AboutDialog;
+}
+
+class AboutDialog : public QDialog
+{
+Q_OBJECT
+  public:
+    AboutDialog(QWidget* parent, const QString& title, const QString& info);
+    ~AboutDialog();
+
+  private:
+    Ui::AboutDialog* m_ui;
+};
+
+#endif // ABOUTDIALOG_HXX
